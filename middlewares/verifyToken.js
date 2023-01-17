93cad7ef2a5370026be6affc207b8c1b67148e2d
AvetikNersisyan <nersisyan.avetik@gmail.com>
2023-01-17 23:50:27 +0400

    hello

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -8,7 +8,7 @@ export const verifyToken = (req, res, next) => {
   const singleToken = tokenBearer || token;
 
   if (!singleToken) {
-    return res.status(403).send("Token is required! barev day");
+    return res.status(403).send("Token is required! barev bari");
   }
 
   try {
