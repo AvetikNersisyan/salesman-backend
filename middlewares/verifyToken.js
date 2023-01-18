575c6d1c26b3111c8d1f36250c0efe90ca8ea01e
Avetik Nersisyan <88989924+AvetikNersisyan@users.noreply.github.com>
2023-01-18 16:30:08 +0400

    Update verifyToken.js

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -8,7 +8,7 @@ export const verifyToken = (req, res, next) => {
   const singleToken = tokenBearer || token;
 
   if (!singleToken) {
-    return res.status(403).send("Token is required! hopar kerpov");
+    return res.status(403).send("Token is required! hopar kerpov 1111");
   }
 
   try {
